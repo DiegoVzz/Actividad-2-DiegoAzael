981e422ba5a22ce03a5598243562a147725aa908
ItzcoatlTO <157817813+ItzcoatlTO@users.noreply.github.com>
2024-01-28 11:52:02 -0600

    Initial Commit

---
+++ Actividad#2 DiegoAzael/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividad#2 DiegoAzael
+//
+//  Created by Itzcoatl on 28/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
